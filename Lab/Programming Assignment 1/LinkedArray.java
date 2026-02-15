a8187e503479304f8fe73489f28b03b7706fe08a
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2026-02-14 21:46:39 -0500

    Add PA1 project skeleton and LinkedArray stub
    Create Programming Assignment 1 directory with IntelliJ project files (.idea/, .iml), assignment PDF (pa1.pdf), and some .DS_Store files. Add LinkedArray.java skeleton (package pa1) containing ArrayNode inner class and unimplemented methods (constructor, get, set, append, pop, size, insert, remove, toString) that currently throw UnsupportedOperationException. Include .idea/.gitignore to ignore IDE artifacts. This is the initial project scaffold for the PA1 assignment.

---
+++ Lab/Programming Assignment 1/LinkedArray.java
@@ -0,0 +1,70 @@
+package pa1;
+
+/**
+ * PUT YOUR INFORMATION BELOW PRIOR TO SUBMISSION
+ * Name: Shrikar Swami
+ * Pledge: I pledge my honor that I have abided by the Stevens Honors System.
+ */
+
+public class LinkedArray {
+    public static class ArrayNode {
+        public String[] array;
+        public ArrayNode next;
+
+        public ArrayNode(int size) {
+            this.array = new String[size];
+        }
+    }
+
+    public ArrayNode head;
+    public int size;
+    public ArrayNode tail;
+
+    public LinkedArray() {
+        throw new UnsupportedOperationException("Constructor Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public String get(int idx) {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public void set(int idx, String value) {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public void append(String value) {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public String pop() {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+    }
+
+    public void insert(int idx, String value) {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+        // This method is OPTIONAL and not for credit.
+    }
+
+    public String remove(int idx) {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+        // This method is OPTIONAL and not for credit.
+    }
+
+    public String toString() {
+        throw new UnsupportedOperationException("Method Not Implemented");
+        // Delete the above line, and write your implementation here.
+        // This method is OPTIONAL and not for credit.
+    }
+}
